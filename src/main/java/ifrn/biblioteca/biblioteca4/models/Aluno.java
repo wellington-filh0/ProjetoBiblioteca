f43565269d99a8b8b66dd714195a6efb0dd6d123
wellington-filh0 <wellingtonfilho721@gmail.com>
2023-10-19 15:51:03 -0300

    Criando modelo para Aluno

---
+++ src/main/java/ifrn/biblioteca/biblioteca4/models/Aluno.java
@@ -0,0 +1,51 @@
+package ifrn.biblioteca.biblioteca4.models;
+
+public class Aluno {
+
+	private String nome;
+	private String matricula;
+	private String cpf;
+	private String nascimento;
+	private String endereco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
