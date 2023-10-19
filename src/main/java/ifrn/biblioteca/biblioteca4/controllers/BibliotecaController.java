ba977d8b222ed6f5bf92a36cce83b2803f047cee
wellington-filh0 <wellingtonfilho721@gmail.com>
2023-10-19 16:43:56 -0300

    Criando a funcionalidade de informar quando o formulário for enviado e mostrar as informações inseridas nele no console

--- src/main/java/ifrn/biblioteca/biblioteca4/controllers/BibliotecaController.java
+++ src/main/java/ifrn/biblioteca/biblioteca4/controllers/BibliotecaController.java
@@ -1,7 +1,11 @@
 package ifrn.biblioteca.biblioteca4.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.biblioteca.biblioteca4.models.Aluno;
 
 @Controller
 public class BibliotecaController {
@@ -9,5 +13,22 @@ public class BibliotecaController {
 	@RequestMapping("/biblioteca/form")
 	public String form() {
 		return "formAluno";
+	}
+	
+	@PostMapping("/submit")
+	public String submetido(Aluno aluno) {
+		System.out.println("Dados do Aluno:");
+		System.out.println("Nome: " + aluno.getNome());
+		System.out.println("Matrícula: " + aluno.getMatricula());
+		System.out.println("Cpf: " + aluno.getCpf());
+		System.out.println("Nascimento: " + aluno.getNascimento());
+		System.out.println("Endereço: " + aluno.getEndereco());
+
+		return "redirect:/success";
+	}
+
+	@GetMapping("/success")
+	public String successPage() {
+		return "success";
 	}
 }
