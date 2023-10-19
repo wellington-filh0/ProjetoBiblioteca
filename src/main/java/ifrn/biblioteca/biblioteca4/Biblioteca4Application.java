9786c7e1eaa3ece4867179fc1ff73b4f4d0a89e0
wellington-filh0 <wellingtonfilho721@gmail.com>
2023-10-19 15:32:02 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ifrn/biblioteca/biblioteca4/Biblioteca4Application.java
@@ -0,0 +1,13 @@
+package ifrn.biblioteca.biblioteca4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblioteca4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biblioteca4Application.class, args);
+	}
+
+}
