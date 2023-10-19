0cad4761fccd9a81a73e64eabc07206d130d09d1
wellington-filh0 <wellingtonfilho721@gmail.com>
2023-10-19 18:56:48 -0300

    Criando repository para Aluno

--- src/main/java/ifrn/biblioteca/biblioteca4/controllers/BibliotecaController.java
+++ src/main/java/ifrn/biblioteca/biblioteca4/controllers/BibliotecaController.java
@@ -1,14 +1,19 @@
 package ifrn.biblioteca.biblioteca4.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.biblioteca.biblioteca4.models.Aluno;
+import ifrn.biblioteca.biblioteca4.repositories.AlunoRepository;
 
 @Controller
 public class BibliotecaController {
+	
+	@Autowired //Define automaticamente a variável abaixo como um objeto
+	private AlunoRepository ar;
 
 	@RequestMapping("/biblioteca/form")
 	public String form() {
@@ -19,6 +24,7 @@ public class BibliotecaController {
 	public String submetido(Aluno aluno) {
 
 		System.out.println(aluno);
+		ar.save(aluno);
 
 		return "redirect:/success";
 	}
